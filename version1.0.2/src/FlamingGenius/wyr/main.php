afc9621bc0d95c8c30a6e0a22ac5ff1d5710a2eb
FlamingGenius <livewiretech.reply.mail@gmail.com>
2016-03-13 01:16:40 -0500

    Update main.php

--- version1.0.2/src/FlamingGenius/wyr/main.php
+++ version1.0.2/src/FlamingGenius/wyr/main.php
@@ -4,7 +4,12 @@ namespace FlamingGenius\wyr;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
+use pocketmine\command\CommandSender;\
+use pocketmine\utils\Config;
+use pocketmine\tile\Sign;
+use pocketmine\event\block\SignChangeEvent;
+use pocketmine\event\Event;
+use pocketmine\event\Player\PlayerInteractEvent;
 
 class main extends PluginBase implements Listener{
 	public function onEnable(){
@@ -15,9 +20,55 @@ class main extends PluginBase implements Listener{
 	private function getQuestion($type){
 		$questions = $this->getConfig()->get($type);
 		$draw = array_rand($questions);
-		
 		$question = $questions[$draw];
+		
 		return $question;
+	}
+	
+	private function signQuestion(SignChangeEvent $event){
+		if($event->getBlock()->getId() == 68 || $event->getBlock()->getId() == 63){
+			$sign = $event->getPlayer()->getLevel()->getTile($event->getBlock());
+			$sign = $event->getLines();
+			if($sign[1] === "Children"){
+				$msg = $this->getQuestion("children-questions");
+				$this->getServer()->broadcastMessage($msg);
+			} elseif($sign[1] === "Adult"){
+				$msg = $this->getQuestion("adult-questions");
+				$this->getServer()->broadcastMessage($msg);
+			} elseif($sign[1] === "Funny"){
+				$msg = $this->getQuestion("funny-questions");
+				$this->getServer()->broadcastMessage($msg);
+			} elseif($sign[1] === "Hard"){
+				$msg = $this->getQuestion("hard-questions");
+				$this->getServer()->broadcastMessage($msg);
+			}
+		}
+	}
+	
+	public function onSignCreate(SignChangeEvent $event){
+		if($event->getBlock()->getId() == 68 || $event->getBlock()->getId() == 63){
+			$sign = $event->getPlayer()->getLevel()->getTile($event->getBlock());
+			$sign = $event->getLines();
+			if($sign[0] === "[WYR]"){
+				if($sign[1] === "children"){
+					$event->setLines(0,"[WYR]");
+					$event->setLines(1,"Children");
+				} elseif($sign[1] === "adult"){
+					$event->setLines(0,"[WYR]");
+					$event->setLines(1,"Adult");
+				} elseif($sign[1] === "funny"){
+					$event->setLines(0,"[WYR]");
+					$event->setLines(1,"Funny");
+				} elseif($sign[1] === "hard"){
+					$event->setLines(0,"[WYR]");
+					$event->setLines(1,"Hard");
+				}
+			}
+		}
+	}
+	
+	public function onSignTouch(PlayerInteractEvent $event){
+		$event->signQuestion();
 	}
 	
 	public function onCommand(CommandSender $sender,Command $command,$label,array $args){
